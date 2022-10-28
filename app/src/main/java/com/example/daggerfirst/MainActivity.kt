842dcd3bb777769a01c060aece9a753056d05ee9
OnlyArjun <arjunyadavcs20@pgdav.du.ac.in>
2022-10-28 21:36:50 +0530

    Update Base UI

---
+++ app/src/main/java/com/example/daggerfirst/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.daggerfirst
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        redirectActivity()
+    }
+
+    private fun redirectActivity() {
+        val mySharedPreferences =
+            getSharedPreferences(getString(R.string.shared_preference_key), MODE_PRIVATE)
+        val currentLoggedInUser = mySharedPreferences.getString(getString(R.string.sp_github_key),null)
+        if(currentLoggedInUser == null) {
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        } else {
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
+        }
+
+
+    }
+
+
+}
